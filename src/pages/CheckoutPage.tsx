f85ca9babffcbadebe392779faaefed636c08980
abhayahlawat <ahlawatabhay21@gmail.com>
2025-09-24 00:53:04 +0530

    update checkout

--- src/pages/CheckoutPage.tsx
+++ src/pages/CheckoutPage.tsx
@@ -64,7 +64,7 @@ const CheckoutPage: React.FC = () => {
         if (!authHeader) return;
         
         const response = await fetch(getApiUrl('api/addresses'), {
-          method: 'GET',
+          method: 'POST',
           headers: { 
             'Authorization': authHeader,
             'Accept': 'application/json'
